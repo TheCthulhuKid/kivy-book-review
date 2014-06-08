5f9c3bc5aa76ce1b00f842014d3688d17090c821
Marc-Anthony Taylor <gileadslostson@gmail.com>
2014-06-09 00:00:16 +0200

    Major refactor of both UI and logic. Added kivy widgets and implemented forecast

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from kivy.app import App
+import json
+import datetime
+from kivy.clock import Clock
 
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.listview import ListItemButton
@@ -6,9 +9,10 @@ from kivy.uix.listview import ListItemButton
 from kivy.properties import ObjectProperty, ListProperty, StringProperty, NumericProperty
 from kivy.network.urlrequest import UrlRequest
 
-from kivy.factory import Factory
+from kivy.storage.jsonstore import JsonStore
+from kivy.uix.modalview import ModalView
 
-import json
+from kivy.factory import Factory
 
 api_url = "http://api.openweathermap.org/data/2.5/"
 
@@ -24,45 +28,96 @@ class LocationButton(ListItemButton):
 
 class WeatherRoot(BoxLayout):
     current_weather = ObjectProperty()
+    locations = ObjectProperty()
+    carousel = ObjectProperty()
+    forecast = ObjectProperty()
+    add_location_form = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.store = JsonStore("weather_store.json")
+        if self.store.exists('locations'):
+            locations = self.store.get('locations')
+            self.locations.locations_list.adapter.data.extend(locations['locations'])
+            current_location = locations["current_location"]
+            self.show_current_weather(current_location)
+        else:
+            Clock.schedule_once(lambda dt: self.show_add_location_form())
 
     def show_add_location_form(self):
-        self.clear_widgets()
-        self.add_widget(AddLocationForm())
+        self.add_location_form = AddLocationForm()
+        self.add_location_form.open()
 
-    def show_current_weather(self, location=None):
-        self.clear_widgets()
+    def show_current_weather(self, location):
+        if location not in self.locations.locations_list.adapter.data:
+            self.locations.locations_list.adapter.data.append(location)
+            self.locations.locations_list._trigger_reset_populate()
+            self.store.put("locations", locations=list(self.locations.locations_list.adapter.data),
+                           current_location=location)
 
-        if self.current_weather is None:
-            self.current_weather = CurrentWeather()
-
-        if location is not None:
-            self.current_weather.location = location
-
+        self.current_weather.location = location
+        self.forecast.location = location
         self.current_weather.update_weather()
-        self.add_widget(self.current_weather)
+        self.forecast.update_weather()
+
+        self.carousel.load_slide(self.current_weather)
+        if self.add_location_form is not None:
+            self.add_location_form.dismiss()
 
 
 class CurrentWeather(BoxLayout):
     location = ListProperty(['Vienna', 'AT'])
     conditions = StringProperty()
+    conditions_image = StringProperty()
     temp = NumericProperty()
     temp_min = NumericProperty()
     temp_max = NumericProperty()
 
     def update_weather(self):
-        weather_template = api_url + "weather?q={},{}&units=metric"
-        weather_url = weather_template.format(*self.location)
+        config = WeatherApp.get_running_app().config
+        temp_type = config.getdefault("General", "temp_type", "metric").lower()
+        weather_template = api_url + "weather?q={},{}&units={}"
+        weather_url = weather_template.format(self.location[0], self.location[1], temp_type)
         request = UrlRequest(weather_url, self.weather_retrieved)
 
     def weather_retrieved(self, request, data):
         data = json.loads(data.decode()) if not isinstance(data, dict) else data
         self.conditions = data['weather'][0]['description']
+        self.conditions_image = "http://openweathermap.org/img/w/{}.png".format(
+            data['weather'][0]['icon'])
         self.temp = data['main']['temp']
         self.temp_min = data['main']['temp_min']
         self.temp_max = data['main']['temp_max']
 
 
-class AddLocationForm(BoxLayout):
+class Forecast(BoxLayout):
+    location = ListProperty(['Vienna', 'AT'])
+    forecast_container = ObjectProperty()
+
+    # TODO: get rid of repeated code
+    def update_weather(self):
+        config = WeatherApp.get_running_app().config
+        temp_type = config.getdefault("General", "temp_type", "metric").lower()
+        weather_template = api_url + "forecast/daily?q={},{}&units={}&cnt=3"  # TODO: settings number of days forecast
+        weather_url = weather_template.format(self.location[0], self.location[1], temp_type)
+        request = UrlRequest(weather_url, self.weather_retrieved)
+
+    def weather_retrieved(self, request, data):
+        data = json.loads(data.decode()) if not isinstance(data, dict) else data
+        self.forecast_container.clear_widgets()
+        for day in data['list']:
+            label = Factory.ForecastLabel()
+            label.date = datetime.datetime.fromtimestamp(day['dt']).strftime("%a %b %d")
+
+            label.conditions = day['weather'][0]['description']
+            label.conditions_image = "http://openweathermap.org/img/w/{}.png".format(
+                day['weather'][0]['icon'])
+            label.temp_min = day['temp']['min']
+            label.temp_max = day['temp']['max']
+            self.forecast_container.add_widget(label)
+
+
+class AddLocationForm(ModalView):
     search_input = ObjectProperty()
     search_results = ObjectProperty()
     location_form = ObjectProperty()
@@ -85,7 +140,28 @@ class AddLocationForm(BoxLayout):
 
 
 class WeatherApp(App):
-    pass
+    def build_config(self, config):
+        config.setdefaults('General', {'temp_type': "metric"})
+
+    def build_settings(self, settings):
+        settings.add_json_panel("Weather Settings",
+                                self.config, data="""
+                                [
+                                    {"type": "options",
+                                        "title": "Temperature System",
+                                        "section": "General",
+                                        "key": "temp_type",
+                                        "options":["Metric", "Imperial"]
+                                    }
+                                ]""")
+
+    def on_config_change(self, config, section, key, value):
+        if config is self.config and key == "temp_type":
+            try:
+                self.root.current_weather.update_weather()
+                self.root.forecast.update_weather()
+            except AttributeError:
+                pass
 
 
 if __name__ == '__main__':
